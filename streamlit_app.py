0c30b45c4299f21f0a92ebf053d6d56981641462
hghdhghd0-tech <hghdhghd0@gmail.com>
2025-08-26 14:40:48 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# 기존 코드: st.title("My First App")
+# 수정 코드:
+st.title("🏫 처음" \
+"")
+st.write("우리 반의 중요한 소식을 확인하세요!")
